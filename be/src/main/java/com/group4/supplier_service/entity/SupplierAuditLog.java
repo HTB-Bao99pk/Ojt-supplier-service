1bebdd5632b1f115273f44881271e6b62f7492cf
ThuThao <thaontse184164@fpt.edu.vn>
2026-02-26 11:09:25 +0700

    Add entity and enums

---
+++ be/src/main/java/com/group4/supplier_service/entity/SupplierAuditLog.java
@@ -0,0 +1,44 @@
+package com.group4.supplier_service.entity;
+
+import com.group4.supplier_service.enums.AuditAction;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "supplier_audit_logs")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class SupplierAuditLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "supplier_id", nullable = false)
+    Supplier supplier;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    AuditAction action = AuditAction.APPROVE;
+
+    @Column(name = "old_data", columnDefinition = "TEXT")
+    String oldData;
+
+    @Column(name = "new_data", columnDefinition = "TEXT")
+    String newData;
+
+    @Column(name = "performed_by", nullable = false)
+    @CreationTimestamp
+    String performedBy;
+
+    @Column(name = "performed_at", updatable = false)
+    LocalDateTime performedAt;
+}
